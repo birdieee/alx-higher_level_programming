ffa38c595f00b571628fc6d9ec10066b1f125a96
birdieee <lolasimbiat@gmail.com>
2023-05-30 01:03:02 -0700

    0x06-python-classes

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+"""The empty class Square defines a square."""
+
+class Square:
+    """It represent a square."""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""class square defines a square."""
+
+class Square:
+
+    """Represents a square."""
+
+    def __init__(self, size):
+        """args:
+        size: size of the square
+        """
+        self.__size = size
