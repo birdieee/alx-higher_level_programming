bd936f9df49bc4266deda5d9db919923e2445c23
birdieee <lolasimbiat@gmail.com>
2023-08-15 12:15:37 -0700

    0x12-javascript-warm_up

---
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const callMeMoby = (x, theFunction) => {
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
+};
+exports.callMeMoby = callMeMoby;
---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+function add (a, b) {
+  const sum = a + b;
+  console.log(sum);
+}
+
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+const num1 = parseInt(arg1);
+const num2 = parseInt(arg2);
+
+if (!isNaN(num1) && !isNaN(num2)) {
+  add(num1, num2);
+} else {
+  console.log('Invalid arguments');
+}
